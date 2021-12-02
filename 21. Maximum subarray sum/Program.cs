86293ee7ba54ccd36837f1235563458acbe3f1c8
captain76 <b_eftimov@yahoo.com>
2021-12-03 00:07:17 +0200

    codewars trainings

---
+++ 21. Maximum subarray sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _21._Maximum_subarray_sum
+{
+    public static class Kata
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            //The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers
+            int totalMaxSum = int.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+               
+                int maxSum = int.MinValue;
+                int currSum = 0;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    currSum += arr[j];
+                    if (currSum>maxSum)
+                    {
+                        
+                        maxSum = currSum;
+
+                    }
+                }
+                if (totalMaxSum<maxSum)
+                {
+                    totalMaxSum = maxSum;
+                }
+            }
+            if (arr.Length>0)
+            {
+                return totalMaxSum;
+            }
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(Kata.MaxSequence(input));
+        }
+    }
+}
---
+++ 22. Prime factors/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _22._Prime_factors
+{
+    public class PrimeDecomp
+    {
+
+        public static String factors(int lst)
+        {
+            Dictionary<int, int> primes = new Dictionary<int, int>();
+            int val2 = 0;
+            while (lst % 2 == 0)
+            {
+                lst /= 2;
+                val2++;
+            }
+            if (val2>0)
+            {
+                primes.Add(2, val2);
+            }
+            
+            
+            for (int i = 3; i <= lst; i+=2)
+            {
+                //bool isPrime = true;
+                int vali = 0;
+                //for (int j = 3; j < Math.Sqrt(i); j++)
+                //{
+                //    if (i%j==0)
+                //    {
+                //        isPrime = false;
+                //        break;
+                //    }
+                //}
+                //if (!isPrime)
+                //{
+                //    continue;
+                //}
+                while (lst%i==0)
+                {
+                    lst /= i;
+                    vali++;
+                }
+                if (vali>0)
+                {
+                    primes.Add(i, vali);
+                }
+                
+            }
+
+
+            string res = "";
+            foreach (var item in primes)
+            {
+                if (item.Value ==1)
+                {
+                    res += $"({item.Key})"; 
+                }
+                else
+                {
+                    res += $"({item.Key}**{item.Value})";
+                }
+            }
+            
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lst = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrimeDecomp.factors(lst));
+        }
+    }
+}
