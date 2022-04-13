cf0b055f0a24f0ec95b89dc3f56018cc4d9ec07c
captain76 <b_eftimov@yahoo.com>
2022-04-13 23:09:06 +0300

    codewars tasks

---
+++ 33 Array.diff/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _33_Array.diff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] { 1, 2, 3, 4, 5, 6 };
+            var b = new int[] { 1, 2, 2 };
+            List<int> c = new List<int>();
+            foreach (var item in a)
+            {
+                if (!b.Any(x=>x==item))
+                {
+                    c.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(", ", c));
+        }
+    }
+}
