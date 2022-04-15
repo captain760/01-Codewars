defaeecaf0d6307a8a44d65247ba2e884223f35e
captain76 <b_eftimov@yahoo.com>
2022-04-15 23:36:21 +0300

    code wars

---
+++ 35 Next bigger number with the same digits/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _35_Next_bigger_number_with_the_same_digits
+{
+    public class Kata
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            char[] n1 = n.ToString().ToCharArray();
+            Array.Sort(n1);
+            string sortedNumberFirst = new string(n1);
+
+                        
+            while (true)
+            {
+                n++;
+                char[] n2 = n.ToString().ToCharArray();
+                Array.Sort(n2);
+                string sortedNumberSecond = new string(n2);
+                if (sortedNumberFirst.Length < sortedNumberSecond.Length)
+                {
+                    return -1;
+                }
+                                
+                if (sortedNumberSecond == sortedNumberFirst)
+                {
+                    return n;
+                }
+                 
+            }                        
+        }
+    }
+}
---
+++ 35 Next bigger number with the same digits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _35_Next_bigger_number_with_the_same_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.NextBiggerNumber(12));
+            Console.WriteLine(Kata.NextBiggerNumber(513));
+            Console.WriteLine(Kata.NextBiggerNumber(2017));
+            Console.WriteLine(Kata.NextBiggerNumber(414));
+            Console.WriteLine(Kata.NextBiggerNumber(144));
+            Console.WriteLine(Kata.NextBiggerNumber(9));
+            Console.WriteLine(Kata.NextBiggerNumber(123111111));
+        }
+    }
+}
