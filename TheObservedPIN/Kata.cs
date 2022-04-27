8c3b24f041ff08f8e8f0d715ff05a26109f870ea
captain76 <b_eftimov@yahoo.com>
2022-04-27 07:03:42 +0300

    codewars exercise

---
+++ TheObservedPIN/Kata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheObservedPIN
+{
+    public class Kata
+    {
+        public static List<string> GetPINs(string observed)
+        {
+            Dictionary<string, int[]> possibleFigures = new Dictionary<string, int[]>
+            {
+                {"1",new[]{1,2,4}},
+                {"2",new[]{1,2,3,5}},
+                {"3",new[]{2,3,6}},
+                {"4",new[]{1,4,5,7}},
+                {"5",new[]{2,4,5,6,8}},
+                {"6",new[]{3,5,6,9}},
+                {"7",new[]{4,7,8}},
+                {"8",new[]{0,5,7,8,9}},
+                {"9",new[]{6,8,9}},
+                {"0",new[]{0,8}}
+            };
+            List<string> result = new List<string>();
+            int numbers = observed.Length;
+            for (int i = 0; i < numbers; i++)
+            {
+                string symbol = observed[i].ToString();
+                int currentFigure = int.Parse(symbol);
+            }
+
+            return null;
+        }
+    }
+}
--- TheObservedPIN/Program.cs
+++ TheObservedPIN/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace TheObservedPIN
 {
+    
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var expectations = new Dictionary<string, string[]>{
+        { "8", new[] { "5", "7", "8", "9", "0" } },
+        {"11",  new[]{"11", "22", "44", "12", "21", "14", "41", "24", "42" } },
+        {"369", new[] { "339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239" } }
+  };
+
+            foreach (var pin in expectations)
+            {
+                CollectionAssert.AreEquivalent(pin.Value, Kata.GetPINs(pin.Key), "PIN: " + pin);
+            }
         }
     }
 }
